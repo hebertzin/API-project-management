2381359c714bfc5cf910149cccaadafe5316c9f7
hebertsanto <hebertsantos0704@gmail.com>
2024-02-15 15:38:38 -0300

    refactor: separate function checkUserAndProjectExistence

--- src/decisions/services/decisions/decisions.service.ts
+++ src/decisions/services/decisions/decisions.service.ts
@@ -6,18 +6,7 @@ import { Decision } from 'src/decisions/types/decision';
 export class DecisionsService {
   constructor(private prismaService: PrismaService) {}
 
-  private async checkUserAndProjectExistence(
-    user_id: string,
-    project_id: string,
-  ) {
-    const user = await this.prismaService.prisma.user.findUnique({
-      where: {
-        id: user_id,
-      },
-    });
-    if (!user) {
-      throw new NotFoundException(`this user ${user_id} does not exist`);
-    }
+  private async checkProjectExistence(project_id: string) {
     const project = await this.prismaService.prisma.projects.findUnique({
       where: {
         id: project_id,
@@ -27,12 +16,20 @@ export class DecisionsService {
       throw new NotFoundException(`this project ${project_id} does not exist `);
     }
   }
+  private async checkUserExistence(user_id: string) {
+    const user = await this.prismaService.prisma.user.findUnique({
+      where: {
+        id: user_id,
+      },
+    });
+    if (!user) {
+      throw new NotFoundException(`this user ${user_id} does not exist`);
+    }
+  }
 
   async create(decision: Decision) {
-    await this.checkUserAndProjectExistence(
-      decision.userId,
-      decision.projectId,
-    );
+    await this.checkUserExistence(decision.userId);
+    await this.checkProjectExistence(decision.projectId);
     const createDecision = await this.prismaService.prisma.decisions.create({
       data: {
         ...decision,
@@ -59,10 +56,8 @@ export class DecisionsService {
     });
   }
   async update(id: string, decision: Decision) {
-    await this.checkUserAndProjectExistence(
-      decision.userId,
-      decision.projectId,
-    );
+    await this.checkUserExistence(decision.userId);
+    await this.checkProjectExistence(decision.projectId);
     const updateDecision = await this.prismaService.prisma.decisions.update({
       where: {
         id,
